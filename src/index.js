150dc0f3113e8d4a019a682279fa8b70dac81356
mrespinosa <104805513+mrespinosa@users.noreply.github.com>
2022-06-28 21:00:23 -0400

    Added API call to replace dummy data in forecast

--- src/index.js
+++ src/index.js
@@ -15,7 +15,13 @@ let hours = now.getHours();
 let minutes = now.getMinutes();
 dateToday.innerHTML = `${day} ${hours}:${minutes}`;
 
-function displayForecast() {
+function getForecast(coordinates) {
+  let apiKey = "d623ebf54f63b0240f122eda2edf85ff";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -63,7 +69,9 @@ function showTemperature(response) {
     "alt",
     `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
   );
+  getForecast(response.data.coord);
 }
+
 function search(city) {
   let apiKey = "d623ebf54f63b0240f122eda2edf85ff";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
@@ -95,6 +103,7 @@ function showCelsiusTemperature(event) {
   let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+
 function showFahrenheitTemperature(event) {
   event.preventDefault();
   fahrenheitLink.classList.add("active");
